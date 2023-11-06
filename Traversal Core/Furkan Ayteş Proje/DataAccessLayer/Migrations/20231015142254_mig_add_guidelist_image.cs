ae75599fbc7bdfd875eeb1cdf9e09dd390e962f4
Furkan <aytesfurkan@gmail.com>
2023-11-06 09:06:06 +0300

    Traversal Core
    .Net Core ile Seyahat Sitesi

---
+++ Traversal Core/Furkan Ayteş Proje/DataAccessLayer/Migrations/20231015142254_mig_add_guidelist_image.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_add_guidelist_image : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
